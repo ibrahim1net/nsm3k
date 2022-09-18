7b20c0564e189adcc151d52850e20a9d664a6313
Ibrahim Alkhudair <35259048+ibrahim1net@users.noreply.github.com>
2022-09-18 21:52:14 +0300

    first commit

---
+++ src/pages/enter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function enter() {
+  return (
+    <div>enter</div>
+  )
+}
+
+export default enter
---
+++ src/pages/home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles/main.css'
+
+function Home() {
+  return (
+    <div className='center-div'>
+      
+
+      <div className='logo-nsm3k'>
+
+
+    
+      </div>
+
+      <div className='box'>
+        <p className='homeFont'>انشاء محادثة</p>
+      </div>
+      <div className='box'>
+        <p className='homeFont'>دخول الى محادثة</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
